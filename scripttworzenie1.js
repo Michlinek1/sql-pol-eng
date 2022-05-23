9d6a28cebb98bcf27baf86a15bb09fb1c3f6ac38
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-05-23 21:30:41 +0200

    Update scripttworzenie1.js

--- scripttworzenie1.js
+++ scripttworzenie1.js
@@ -21,19 +21,14 @@ function zapisz_tabele(){
     }else if (pierwszy_record[3] == ' ' || pierwszy_record[3] <= '0'){
         alert("rozmiar nie może się równać 0 ");
     }else{
-        if(linijki[0] == "STWÓRZ BAZĘ DANYCH " + nazwa_tabeli){
-            var translacja_baza_danych = "CREATE DATABASE " + nazwa_tabeli;
-            var blob = new Blob([translacja_baza_danych], {type: "text/plain;charset=utf-8"});
-        }else{
-            var translacja_table = "CREATE TABLE " + nazwa_tabeli + "(" + "\n" + pierwszy_record_nazwa + " " + pierwszy_record[1] +'(' +pierwszy_record[3] + ')' + "\n" + ")";
-            var blob = new Blob([translacja_table], {type: "text/plain;charset=utf-8"});
-        }
-        const href = window.URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = href;
-        link.download = nazwa_tabeli+".sql";
-        document.body.appendChild(link);
-        link.click();
+var translacja_table = "CREATE TABLE " + nazwa_tabeli + "(" + "\n" + pierwszy_record_nazwa + " " + pierwszy_record[1] +'(' +pierwszy_record[3] + ')' + "\n" + ")";
+var blob = new Blob([translacja_table], {type: "text/plain;charset=utf-8"});
+const href = window.URL.createObjectURL(blob);
+const link = document.createElement("a");
+link.href = href;
+link.download = nazwa_tabeli+".sql";
+document.body.appendChild(link);
+link.click();
 
 
     }
